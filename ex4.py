2928da50d8acba68a5d67f9de7b9b16c2022e206
unknown <petrut1997@yahoo.com>
2017-10-05 00:03:53 +0300

    Finally

---
+++ ex4.py
@@ -0,0 +1,15 @@
+my_name = 'Vasile Petrut'
+my_age = 20 # maybe
+my_height = 50 # inches
+my_weight = 190 # pounds
+my_eyes = 'Blue'
+my_hair = 'Black'
+is_heavy = my_weight > 3000
+print(f"Let's talk about {my_name}.")
+# swap in your preferred pronoun.
+print(f"He is {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"It is {is_heavy} that he is overweight.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
