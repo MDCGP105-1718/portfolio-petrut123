50f92c9e8807a042f7390e2347ae671446143bc9
josephbarber96 <Joseph_Barber@Hotmail.co.uk>
2017-10-05 10:59:16 +0100

    Random stuff

---
+++ ex5.py
@@ -0,0 +1,6 @@
+i=0
+
+for i in range(99,-1,-1):
+    print(f"{i} bottles of beer on the wall, {i} bottles of beer.Take one down, pass it around, {i-1} bottles of beer on the wall… ")
+
+print("No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall")
---
+++ ex6.py
@@ -0,0 +1,21 @@
+Name=input("Put your name in ")
+Age=input("Age ")
+Height=input("Height in cm ")
+Weight=input("Weight in kg ")
+EyeColour=input("Eye colour ")
+HairColour=input("Hair colour ")
+
+if int(Age)>=55:
+    print("Eldery")
+if int(Age)>=70:
+    print("You will die")
+
+if int(Height)>=220:
+    print("NBA")
+if int(Height)<=150:
+    print("Smurf")
+
+if int(Weight)>=90:
+    print("You need to lose wight")
+if int(Weight)<=55:
+    print("EAT SOME FOOD")
