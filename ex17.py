8572800cce08d0bc08036e6451ad3d5e533fc3bd
JosephBarber96 <Joseph_Barber@Hotmail.co.uk>
2017-11-16 11:15:03 +0000

    Finished this time!

--- ex17.py
+++ ex17.py
@@ -1,2 +1,8 @@
 class car(object):
-    def __init__(self)
+    def __init__(self,colour,manufacturer,model,doors):
+        self.colour = colour;
+        self.manufacturer = manufacturer;
+        self.model = model;
+        self.doors = doors;
+    def __str__(self):
+        return car(self.colour, self.manufacturer, self.model, self.doors)
