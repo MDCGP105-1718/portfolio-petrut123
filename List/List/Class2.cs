446c8cade7cc0b034f94f202144b1228381c2255
JosephBarber96 <Joseph_Barber@Hotmail.co.uk>
2018-02-15 11:04:26 +0000

    Stack class

---
+++ List/List/Class2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    
+    class Stack
+    {
+        int Counter = 0;
+
+        private LinkedList stack;
+
+
+        public LinkedList CreateStack()
+        {
+            LinkedList s = new LinkedList();
+            return s;
+        }
+        public LinkedList DestroyStack(LinkedList stack)
+        {
+            while (stack != null)
+            {
+                stack.RemoveBeginning();
+            }
+            return stack;
+        }
+        public LinkedList Pop(LinkedList stack)
+        {
+            object Data;
+            if (stack.GetFront() != null)
+            {
+                Data = stack.GetFront();
+                stack.RemoveBeginning();
+                Counter--;
+            }
+            return stack;
+        }
+        public LinkedList Push(LinkedList stack, object Data)
+        {
+            stack.InsertBeginning(Data);
+            Counter++;
+            return stack;
+        }
+        public bool IsEmpty(LinkedList stack, object Data)
+        {
+            if (stack.GetFront() == null)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        public object Peek(LinkedList stack)
+        {
+            return  stack.GetFront();
+        }
+        public int Count(LinkedList stack)
+        {
+            return Counter;
+        }
+    }
+}
--- List/List/Program.cs
+++ List/List/Program.cs
@@ -12,15 +12,8 @@ namespace List
         {
             LinkedList list = new LinkedList();
             
-            list.InsertBeginning(1);
-            list.InsertBeginning(2);
-            list.InsertBeginning(3);
-            list.InsertBeginning(4);
-            list.RemoveAfter(2);
-            list.Length();
-            Console.WriteLine();
-            list.PrintList();
-            Console.ReadLine();
+              
+           
         }
     }
 }
